d60131719cbb951ade09aa687e6d8dfbf70f6819
RituRaj Singh <frostc010@gmail.com>
2023-11-29 09:23:03 +0530

    helo

---
+++ Book-Store-MERN-Stack-main/backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://frostc010:H3tlH6Pcts3s4DBw@cluster0.chviffk.mongodb.net/?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
